b541a87824701a1d24699a5c4ff7981ebe84879a
Jeet sadhukhan <jeetsadhukhan75992@gmail.com>
2025-06-16 23:07:45 +0530

    3nd

--- src/admin/Courses/AdminCourses.jsx
+++ src/admin/Courses/AdminCourses.jsx
@@ -3,7 +3,7 @@ import Layout from '../Utils/Layout'
 import { useNavigate } from 'react-router-dom'
 import { CourseData } from '../../context/CourseContext';
 import CourseCard from '../../components/coursecard/CourseCard';
-import "./adminCourses.css";
+import "./admincourses.css";
 import toast from 'react-hot-toast';
 import axios from 'axios';
 import { server } from '../../main';
